d6dcd558f92141956fd5b28e6fd2d9f076e90403
Muideen Popoola <popoolamuideen61@gmail.com>
2024-12-09 03:43:02 +0100

    Implement the getUser endpoint

---
+++ src/features/authentication/useUser.js
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import { getCurrentUser } from '../../services/apiAuth';
+
+export const useUser = () => {
+  const { data: user, isPending: isLoading } = useQuery({
+    queryKey: ['user'],
+    queryFn: getCurrentUser,
+  });
+
+  return {
+    user,
+    id: user?._id,
+    isLoading,
+    isAuthenticated: user?.createdAt,
+    email: user?.email,
+  };
+};
